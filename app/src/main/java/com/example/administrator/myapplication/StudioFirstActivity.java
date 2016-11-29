65ce095a779286af0e1c0b948045fd7ffc01f0d5
Administrator <346569107@qq.com>
2016-11-29 17:32:13 +0800

    初始提交

---
+++ app/src/main/java/com/example/administrator/myapplication/StudioFirstActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudioFirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // commit
+        setContentView(R.layout.activity_studio_first);
+    }
+}
