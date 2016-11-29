47928dcdd87417aa4481b74d68c5eaec55b16495
Administrator <346569107@qq.com>
2016-11-29 17:35:25 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/administrator/myapplication/TestActivity.java
@@ -0,0 +1,11 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by witer on 2016/11/29.
+ */
+
+public class TestActivity extends Activity {
+}
